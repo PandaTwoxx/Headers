332809effbc26549329fca51a1b34a8169153ce2
Weston Gorczyca <weston.gorczyca@gmail.com>
2024-07-12 09:19:19 -0400

    bug fix, replaces flask run with waitress serve

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from waitress import serve
 
 app = Flask(__name__)
 
@@ -9,4 +10,4 @@ def index():
   return render_template('index.html', headers=headers)
 
 if __name__ == '__main__':
-  app.run(debug=False, port=8000, host='0.0.0.0')
+  serve(app, host='0.0.0.0', port='8000')
